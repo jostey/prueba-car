cfac1a3457b9c52c306db039fc5539c45d5738a5
FranGilA <jostey96@gmail.com>
2019-10-04 12:48:22 +0200

    Estructura básica Services

--- src/main/java/com/curso/repository/CarRepository.java
+++ src/main/java/com/curso/repository/CarRepository.java
@@ -1,9 +1,11 @@
 package com.curso.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.curso.model.CarEntity;
 
+@Repository
 public interface CarRepository extends JpaRepository<CarEntity, Integer> {
 
 }
--- src/main/java/com/curso/repository/RentRepository.java
+++ src/main/java/com/curso/repository/RentRepository.java
@@ -1,9 +1,11 @@
 package com.curso.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.curso.model.RentEntity;
 
+@Repository
 public interface RentRepository extends JpaRepository<RentEntity, Integer>{
 
 }
