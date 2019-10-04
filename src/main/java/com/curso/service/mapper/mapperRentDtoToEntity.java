bac996e9b416fa7df1c07ab21c325885f77537c6
FranGilA <jostey96@gmail.com>
2019-10-04 13:47:40 +0200

    Cambios en los Mappers

--- src/main/java/com/curso/service/mapper/mapperRentDtoToEntity.java
+++ src/main/java/com/curso/service/mapper/mapperRentDtoToEntity.java
@@ -1,10 +1,14 @@
 package com.curso.service.mapper;
 
 
+import java.time.LocalDate;
+
 import org.springframework.stereotype.Service;
 
 import com.curso.dto.RentDto;
+import com.curso.model.CarEntity;
 import com.curso.model.RentEntity;
+import com.curso.model.UserEntity;
 
 @Service
 public class mapperRentDtoToEntity implements MapperService<RentDto, RentEntity>{
@@ -12,11 +16,12 @@ public class mapperRentDtoToEntity implements MapperService<RentDto, RentEntity>
 	@Override
 	public RentEntity map(RentDto i) {
 		
-		//ZonedDateTime initDate = ZonedDateTime.ofInstant(Instant.ofEpochSecond(i.getInitDate()), ZoneId.of ("Europe/Madrid"));
-		//ZonedDateTime finalDate = ZonedDateTime.ofInstant(Instant.ofEpochSecond(i.getFinalDate()), ZoneId.of ("Europe/Madrid"));
-		
+		UserEntity user = new UserEntity(i.getUser().getId(), i.getUser().getName());
 				
-		return null;
+		return new RentEntity(i.getId(), 
+				user, 
+				new CarEntity(i.getCar().getId(), i.getCar().getModel(), i.getCar().getBrand(), user), 
+				LocalDate.ofEpochDay(i.getInitDate()), LocalDate.ofEpochDay(i.getFinalDate()), i.getPrice());
 	}
 	
 }
--- src/main/java/com/curso/service/mapper/mapperRentEntityToDto.java
+++ src/main/java/com/curso/service/mapper/mapperRentEntityToDto.java
@@ -3,7 +3,9 @@ package com.curso.service.mapper;
 import org.springframework.stereotype.Service;
 
 import com.curso.dto.RentDto;
+import com.curso.dto.UserDto;
 import com.curso.model.RentEntity;
+import com.curso.model.UserEntity;
 
 @Service
 public class mapperRentEntityToDto implements MapperService<RentEntity, RentDto>{
@@ -11,6 +13,10 @@ public class mapperRentEntityToDto implements MapperService<RentEntity, RentDto>
 	@Override
 	public RentDto map(RentEntity i) {
 		//return new RentDto(i.getId(), i.getUser(), i.getCar(), Integer.valueOf(i.getInitDate().toEpochDay()), finalDate, price)
+		UserDto user = new UserDto(i.getUser().getId(), i.getUser().getName());
+		
+//		new RentDto(id, user, car, initDate, finalDate, price)
+//		return new RentDto(i.getId(), user, new CarDto(), );
 		
 		return null;
 	}
